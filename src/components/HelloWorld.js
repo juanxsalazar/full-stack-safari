775872d80dee09a29a19d50f020b564eb6483eea
juanxsalazar <juanxsalazar21@gmail.com>
2019-03-26 18:29:25 -0400

    can search api by location on front end

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react'
 class HelloWorld extends Component {
 
   state = {
-    animals: []
+    animals: [],
+    name: ''
   }
+
 
   componentDidMount() {
     fetch('http://localhost:3000/animals')
@@ -14,12 +16,29 @@ class HelloWorld extends Component {
     })
   }
 
+  SearchChange = event => {
+    const name = event.target.value
+
+    this.setState({ name: name }, () => {
+      fetch(`http://localhost:3000/animals?location=${this.state.name}`)
+      .then(response => response.json())
+      .then(response => {
+        this.setState({ animals: response }) 
+      })
+    })
+  }
+  
   render() {
     return <>
-    <p>Animal Species:</p>
+    <p>Animal Species Search by location:</p>
+<input 
+value={this.state.name} 
+onChange={this.SearchChange}
+type="text" 
+placeholder="search..." />
     <ul>
      {this.state.animals.map(animal => {
-       return <li key={animal.id}>{animal.species}</li>
+       return <li key={animal.id}>A {animal.species} can be seen at {animal.last_seen_location} <p>It has been seen {animal.seen_count} times</p></li>
      })}
     </ul>
     </>
