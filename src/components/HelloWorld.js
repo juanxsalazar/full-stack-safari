269e483fb572d34b1cd4b32a9f0ef09296f9402e
juanxsalazar <juanxsalazar21@gmail.com>
2019-03-26 15:42:17 -0400

    added fetch and localhost api species list

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react'
 
 class HelloWorld extends Component {
+
+  state = {
+    animals: []
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/animals')
+    .then(response => response.json())
+    .then(response => {
+      this.setState ({ animals: response}) 
+    })
+  }
+
   render() {
-    return <h1>Hello, World!</h1>
+    return <>
+    <p>Animal Species:</p>
+    <ul>
+     {this.state.animals.map(animal => {
+       return <li key={animal.id}>{animal.species}</li>
+     })}
+    </ul>
+    </>
   }
 }
 
